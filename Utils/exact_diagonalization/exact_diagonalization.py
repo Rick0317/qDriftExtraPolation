596bdc5bb6d03dfaa8f8141c7585090614922694
Rick0317 <rick.hey.huang@gmail.com>
2024-06-26 18:07:54 -0400

    Testing on ED groundstate energy finding

--- Utils/exact_diagonalization/exact_diagonalization.py
+++ Utils/exact_diagonalization/exact_diagonalization.py
@@ -1,5 +1,7 @@
 import Utils.fermion_util.openfermion_util as of_util
 import openfermion as of
+import numpy as np
+import scipy
 
 
 def ed_ground_state(obt, tbt, spin_orb=True):
@@ -15,3 +17,14 @@ def ed_ground_state(obt, tbt, spin_orb=True):
     sparse_op = of.get_sparse_operator(of_hamil)
     gse, gs = of.get_ground_state(sparse_op)
     return gse, gs
+
+
+if __name__ == "__main__":
+    sample_obt = np.array([[1.0, 0.0], [0.0, 1.0]])
+    tbt = np.zeros((2, 2, 2, 2))
+    obt = of_util.get_ferm_op_one(sample_obt, spin_orb=True)
+    print(obt)
+    sparse_op = scipy.sparse.csr_matrix(sample_obt)
+    print(sparse_op)
+    gse, gs = of.linalg.get_ground_state(sparse_op)
+    print(gse)
