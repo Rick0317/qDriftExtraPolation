d239e134b7ed98d7e6b80901f9466a441a52e144
Jiheon lee <jhlee520801@gmail.com>
2024-06-28 21:59:39 -0400

    finished qdrift

--- scripts/algo/qdrift.py
+++ scripts/algo/qdrift.py
@@ -1,8 +1,10 @@
 import math
 from scipy import linalg  # for exponentials of matrices
+
+from scripts.database import DataManager
 from scripts.database.data_interface import *
 
-t = 6000
+t = 1
 
 
 class HamiltonianSampling:
@@ -23,7 +25,7 @@ class HamiltonianSampling:
         return np.random.choice(np.array(self.h.get_decomp().lst_Hamil), p=self.pk)
 
 
-def qdrift(hubbard: Hubbard, sample, epsilon: float):
+def qdrift(hubbard: Hubbard, epsilon: float):
     """The qDrift protocol. The variable names follow the definition in the "Random Compiler for Fast Hamiltonian Simulation" paper.
     
     :param hubbard: A Hubbard hamiltonian
@@ -31,7 +33,6 @@ def qdrift(hubbard: Hubbard, sample, epsilon: float):
     :param epsilon: target precision
     :return: v_list: a list of sampled unitaries of the exponential form
     """
-    hubbard.decompose(['obt', 'tbt'])
     sample = HamiltonianSampling(hubbard).sample
     lm = hubbard.get_decomp().sum_coeff
     N = math.ceil(2 * (lm ** 2) * (t ** 2) / epsilon)
@@ -43,3 +44,12 @@ def qdrift(hubbard: Hubbard, sample, epsilon: float):
         v_list.append(linalg.expm(1j * lm * t * j.matrix / N))
 
     return v_list
+
+
+if __name__ == "__main__":
+    data = DataManager()  # Create DataManager instance
+
+    ld = data.load('hubbard', "h_2")  # load the hubbard model
+    v = qdrift(ld, 0.01)
+
+    print(v)
