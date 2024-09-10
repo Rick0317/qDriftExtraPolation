4d2957ae2fdaadee4c3819d6b5cfad32478609c5
Rodrigo.ReyesFeregrino <rodrigo.reyesferegrino@sanofi.com>
2024-09-10 14:08:59 -0400

    Numerical data for Hoeffdings ineq bound on estimation of P(0)

--- scripts/error_analysis/phase_estimation.py
+++ scripts/error_analysis/phase_estimation.py
@@ -6,6 +6,7 @@ import scipy
 import json
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import tqdm
 import openpyxl
 from openpyxl.styles import PatternFill
@@ -33,73 +34,119 @@ def generate_random_hermitian(print_output=False):
                 print(f'Eigenvalues: {eigenvalues}')
             return hermitian_matrix
         
+
+def probability_of_estimation_error(delta, n):
+    # P [ˆθ − θ ≥ δ ] ≤ 2e^{−2nδ^2}
+    # returns upper bound for probability that the estimation error is greater than delta
+    return 2 * np.exp(-2 * n * delta**2)
+        
 if __name__ == "__main__":
 
-    NUM_TRIALS = 100
-    PHASE_ESTIMATION_MEAUREMENTS = 10000
-    NUM_RAND_MATRICES = 10
+    NUM_TRIALS = 10000
+    NUM_RAND_MATRICES = 15
     backend = Aer.get_backend('qasm_simulator')
     random_hermitians = [generate_random_hermitian() for _ in range(NUM_RAND_MATRICES)]
-    all_estimation_errors = {}
+    all_estimation_errors = {} 
+    
+    for i in [5,50,5000,10000]:
+        PHASE_ESTIMATION_MEAUREMENTS = i
+        all_p0_estimation_errors = []
+        fig = go.Figure()
 
-    for i, test_hermitian in tqdm.tqdm(enumerate(random_hermitians), desc='Running phase estimation on random Hermitian matrices', total=NUM_RAND_MATRICES):
+        for i, test_hermitian in tqdm.tqdm(enumerate(random_hermitians), desc='Running phase estimation on random Hermitian matrices', total=NUM_RAND_MATRICES):
 
-        exact_eigenvalue, eigenvectors= scipy.linalg.eig(test_hermitian)
-        U = scipy.linalg.expm(1j * test_hermitian) # unitary operator corresponding to the Hermitian matrix
-        eigenstate = eigenvectors[:, np.argmin(exact_eigenvalue)] # pick eigenstate that corresponds to smallest eigenvalue
-        estimated_eigenvalues = []
-        estimation_errors = []
+            exact_eigenvalue, eigenvectors= scipy.linalg.eig(test_hermitian)
+            exact_p0 = np.cos(min(exact_eigenvalue) / 2) ** 2 # we kind of arbitrarily choose the smallest eigenvalue as the one we want to estimate
+            U = scipy.linalg.expm(1j * test_hermitian) # unitary operator corresponding to the Hermitian matrix
+            eigenstate = eigenvectors[:, np.argmin(exact_eigenvalue)] # pick eigenstate that corresponds to smallest eigenvalue
+            
+            estimated_eigenvalues = []
+            estimation_errors = []
+            p0_estimation_errors = []
 
-        for _ in range(NUM_TRIALS):
-            p_0, qc = iterative_phase_estimation_v2(U=U, eigenstate=eigenstate, num_evals=PHASE_ESTIMATION_MEAUREMENTS, backend=backend, num_qubits=1)
-            # print(p_0)
-            estimated_eigenvalue = 2 * np.arccos(np.sqrt(p_0)) # convert estimated probability of measuring 0 to estimated eigenvalue
-            estimated_eigenvalues.append(estimated_eigenvalue)
-            estimation_errors.append(np.abs(estimated_eigenvalue - min(exact_eigenvalue))) # error = difference between smallest true eigenvalue and estimated eigenvalue
+            for _ in range(NUM_TRIALS):
+
+                p_0, qc = iterative_phase_estimation_v2(U=U, eigenstate=eigenstate, num_evals=PHASE_ESTIMATION_MEAUREMENTS, backend=backend, num_qubits=1)
+                estimated_eigenvalue = 2 * np.arccos(np.sqrt(p_0)) # convert estimated probability of measuring 0 to estimated eigenvalue
+                estimated_eigenvalues.append(float(estimated_eigenvalue))
+                estimation_errors.append(np.abs(estimated_eigenvalue - min(exact_eigenvalue))) # error = difference between smallest true eigenvalue and estimated eigenvalue
+                p0_estimation_errors.append(np.abs(p_0 - exact_p0))
+
+            all_p0_estimation_errors.extend(p0_estimation_errors)
+
+            # Define the number of bins and the bin edges
+            num_bins = 100
+            bin_edges = np.linspace(0, 1, num_bins + 1)
+
+            # Manually bin the data
+            hist, bin_edges = np.histogram(p0_estimation_errors, bins=bin_edges)
+
+            # Normalize the bin counts with respect to the most frequent bin
+            max_count = np.max(hist)
+            relative_freq = hist / max_count
+
+            # Add the bar plot for this matrix
+            fig.add_trace(go.Bar(x=bin_edges[:-1], y=relative_freq, name=f'Matrix {i + 1}'))
+
+            print(f'Hermitian matrix {i + 1} / {NUM_RAND_MATRICES}:\n{test_hermitian} \nAverage estimation error: {np.mean(estimation_errors)}\n\n')
+            all_estimation_errors[i] = estimation_errors
+            
+        # Generate data for the function
+        delta_values = np.linspace(-0.00001, 1, 100)
+        prob_values = probability_of_estimation_error(delta_values, n=PHASE_ESTIMATION_MEAUREMENTS)
+
+        # Add the function plot
+        fig.add_trace(go.Scatter(x=delta_values, y=prob_values, mode='lines', name='Probability of Estimation Error'))
         
-        print(f'Hermitian matrix {i + 1} / {NUM_RAND_MATRICES}:\n{test_hermitian} \nAverage estimation error: {np.mean(estimation_errors)}\n\n')
-        all_estimation_errors[i] = estimation_errors
 
-    # Write all_estimation_errors to json file
-    with open('estimation_errors.json', 'w') as f:
-        json.dump(all_estimation_errors, f)
-    print('All estimation errors written to estimation_errors.json')
+        # Add label for n
+        fig.add_annotation(
+            x=0.5,
+            y=0.5,
+            text=f'n = {PHASE_ESTIMATION_MEAUREMENTS}',
+            showarrow=False,
+            font=dict(size=16)
+        )
+        # Add label for number of trials
+        fig.add_annotation(
+            x=0.5,
+            y=0.4,
+            text=f'Number of trials = {NUM_TRIALS}',
+            showarrow=False,
+            font=dict(size=16)
+        )
 
-    df = pd.DataFrame(columns=['Matrix', 'True Eigenvalue', 'Average Estimation Error', 'Max Estimation Error'])
+        # Update the layout
+        fig.update_layout(
+            title=f'Relative frequency of p_0 estimation errors for PHASE_ESTIMATION_MEASUREMENTS = {PHASE_ESTIMATION_MEAUREMENTS}',
+            xaxis_title='p_0 estimation error',
+            yaxis_title='Relative Frequency',
+            xaxis=dict(range=[0, 0.5])
+        )
+
+        # Save the plot as a png
+        fig.write_image(f'p0_estimation_errors_{PHASE_ESTIMATION_MEAUREMENTS}.png')
+        fig.show()
+
+
+
+
+
+    '''
+    # Create a pandas dataframe to store the results
+    df = pd.DataFrame(columns=['Matrix', 'True Eigenvalue', 'Average Estimation Error', 'Max Estimation Error', 'Variance in MLE estimates of p_0', 'Fisher Information lower Bound'])
     for i, (test_hermitian, estimation_errors) in enumerate(zip(random_hermitians, all_estimation_errors.values())):
         exact_eigenvalue, _ = scipy.linalg.eig(test_hermitian)
-        df.loc[i] = [test_hermitian, min(exact_eigenvalue), np.mean(estimation_errors), max(estimation_errors)]
+        df.loc[i] = [test_hermitian, min(exact_eigenvalue), np.mean(estimation_errors), max(estimation_errors), all_estimation_variances[i][0], all_estimation_variances[i][1]]
     
     # Write df to excel file
     with pd.ExcelWriter('estimation_errors.xlsx') as writer:
         df.to_excel(writer, sheet_name='Estimation Errors', index=False)
         workbook = writer.book
         worksheet = writer.sheets['Estimation Errors']
-        red_fill = PatternFill(start_color='FFFF0000', end_color='FFFF0000', fill_type='solid')
-        for i in range(2, len(df) + 2):
-            worksheet[f'C{i}'].fill = red_fill
-            worksheet[f'D{i}'].fill = red_fill
-            worksheet[f'E{i}'].fill = red_fill
-
-    # Create a histogram of the estimation errors
-    all_errors = [error for errors in all_estimation_errors.values() for error in errors]
-    fig_hist = px.histogram(all_errors, nbins=30, title='Histogram of Estimation Errors')
-    fig_hist.update_layout(xaxis_title='Estimation Error', yaxis_title='Frequency')
-    fig_hist.show()
-
-    # Create scatter plots
-    true_eigenvalues = [min(scipy.linalg.eig(matrix)[0]) for matrix in random_hermitians]
-    average_errors = [np.mean(errors) for errors in all_estimation_errors.values()]
-    max_errors = [max(errors) for errors in all_estimation_errors.values()]
-
-    fig_avg = px.scatter(x=true_eigenvalues, y=average_errors, title='True Eigenvalues vs Average Estimation Errors')
-    fig_avg.update_layout(xaxis_title='True Eigenvalue', yaxis_title='Average Estimation Error')
-    fig_avg.show()
-
-    fig_max = px.scatter(x=true_eigenvalues, y=max_errors, title='True Eigenvalues vs Max Estimation Errors')
-    fig_max.update_layout(xaxis_title='True Eigenvalue', yaxis_title='Max Estimation Error')
-    fig_max.show()
-
+    '''
+    
+    
 
 
 
